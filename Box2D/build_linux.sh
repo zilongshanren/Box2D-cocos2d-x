3e94cb07a024de0248920c05fa8c809b453554b4
andyque <guanghui8827@126.com>
2014-08-19 05:00:57 -0700

    finish linux scripts

--- Box2D/build_linux.sh
+++ Box2D/build_linux.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+# in order to build 32 bit application on 64bit machine
+# we have to install the following packages
+# and also we should specify the CXX_FLAGS = m32
 sudo apt-get install libx32gcc-4.8-dev
 
 sudo apt-get install libc6-dev-i386
+
+sudo apt-get install lib32stdc++6
+
+sudo apt-get install g++-multilib
 
 rm -rf prebuilt
 
@@ -9,14 +16,14 @@ cmake -DLINUX32=1 .
 
 make
 
-# mkdir -p prebuilt/32/
-# cp lib/libBox2D.a prebuilt/32/
-# rm -rf lib
+mkdir -p prebuilt/32/
+cp lib/libBox2D.a prebuilt/32/
+rm -rf lib
 
-# cmake -DLINUX64=1 .
+cmake -DLINUX32=0 .
 
-# make
+make
 
 
-# mkdir -p prebuilt/64/
-# cp lib/libBox2D.a prebuilt/64/
+mkdir -p prebuilt/64/
+cp lib/libBox2D.a prebuilt/64/
