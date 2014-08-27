d6ae577e4cdb75aa3e0c0a15d2f581024b2906b4
andyque <guanghui8827@126.com>
2014-08-27 16:17:59 +0800

    add ios-cmake toolchain support

--- Box2D/build_iosmac.sh
+++ Box2D/build_iosmac.sh
@@ -1,3 +1,7 @@
 xcodebuild -project Box2D.xcodeproj -alltargets -sdk iphonesimulator7.1 -configuration Release
 xcodebuild -project Box2D.xcodeproj -alltargets -sdk iphoneos7.1 -configuration Release
 lipo build/Release-iphoneos/libBox2D.a build/Release-iphonesimulator/libBox2D.a -create -output libBox2D.a
+
+xcodebuild -project Project.xcodeproj -alltargets  -arch x86_64 -configuration Release
+
+
