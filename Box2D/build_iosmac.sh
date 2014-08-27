c4afa450d152b92e5491a8cef92657f016d5efb8
andyque <guanghui8827@126.com>
2014-08-27 17:29:24 +0800

    add ios_32bit and 64bit toolchain

--- Box2D/build_iosmac.sh
+++ Box2D/build_iosmac.sh
@@ -7,20 +7,60 @@
 
 # use ios-cmake to generate ios project
 rm -rf build.ios
+rm libBox2D.a
 mkdir build.ios
 cd build.ios
 
-#build for ios arm64
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/iOS.cmake  -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/ -DIOS_PLATFORM=OS -GXcode ..
+#generate ios project
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/iOS_32.cmake  -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/  -GXcode ..
 
-# return to the uplevel folder
-cd  .. 
-xcodebuild -project build.ios/Project.xcodeproj -alltargets -sdk iphonesimulator7.1 -configuration Release
-# xcodebuild -project build.ios/Project.xcodeproj -alltargets -sdk iphoneos7.1 -configuration Release
+
+# build iphone simulator
+xcodebuild -project Project.xcodeproj -alltargets -sdk iphonesimulator7.1 -configuration Release
+
+cd ..
+mkdir -p lib/i386
+cp build.ios/lib/Release/libbox2d.a lib/i386
+
+
+# build iphone os, this is a fat lib
+xcodebuild -project build.ios/Project.xcodeproj -alltargets -sdk iphoneos7.1 -configuration Release
+
+mkdir -p lib/armv7
+cp build.ios/lib/Release/libbox2d.a lib/armv7
+
+rm -rf build.ios
+mkdir build.ios
+cd build.ios
+
+#generate ios project
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/iOS_64.cmake  -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/  -GXcode ..
+
+
+# build iphone simulator 64 bit
+xcodebuild -project Project.xcodeproj -alltargets -sdk iphonesimulator7.1 -configuration Release
+
+cd ..
+mkdir -p lib/x86_64
+cp build.ios/lib/Release/libbox2d.a lib/x86_64
+
+
+# build iphone os, this is a fat lib
+xcodebuild -project build.ios/Project.xcodeproj -alltargets -sdk iphoneos7.1 -configuration Release  
+
+mkdir -p lib/arm64
+cp build.ios/lib/Release/libbox2d.a lib/arm64
+
 # create the fat package
-# lipo build/Release-iphoneos/libBox2D.a build/Release-iphonesimulator/libBox2D.a -create -output libBox2D.a
+lipo lib/i386/libbox2d.a lib/x86_64/libbox2d.a lib/armv7/libbox2d.a lib/arm64/libbox2d.a -create -output libBox2D.a
+lipo -info libBox2D.a
 
+mkdir -p prebuilt/ios
+mv libBox2D.a prebuilt/ios
+rm -rf lib
+rm -rf build.ios
 
+echo "finished build ios fat library"
 
 
 # xcodebuild -project Project.xcodeproj -alltargets  -arch x86_64 -configuration Release
