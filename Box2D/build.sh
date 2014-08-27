4b246bd804849ce7eab4e6f9de79ac2ff101e57d
andyque <guanghui8827@126.com>
2014-08-27 10:40:09 +0800

    add box2d xcode project

---
+++ Box2D/build.sh
@@ -0,0 +1,3 @@
+xcodebuild -project Box2D.xcodeproj -alltargets -sdk iphonesimulator7.1 -configuration Release
+xcodebuild -project Box2D.xcodeproj -alltargets -sdk iphoneos7.1 -configuration Release
+lipo build/Release-iphoneos/libBox2D.a build/Release-iphonesimulator/libBox2D.a -create -output libBox2D.a
