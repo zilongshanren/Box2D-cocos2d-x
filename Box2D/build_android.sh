2ce67b0988be4d2eab76584a61b79a41872f763d
andyque <guanghui8827@126.com>
2014-08-31 16:30:22 +0800

    finish Box2D android.toolchain.cmake configs

--- Box2D/build_android.sh
+++ Box2D/build_android.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 
-export ANDROID_NDK_TOOLCHAIN_ROOT=/Users/guanghui/AndroidDev/android-ndk-r9d/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
-export ANDROID_NDK=/Users/guanghui/AndroidDev/android-ndk-r9d/
+export PATH=$PATH:$HOME/Github/android-toolchain/bin
+# export ANDROID_NDK_TOOLCHAIN_ROOT=$HOME/Github/android-toolchain
+export ANDROID_STANDALONE_TOOLCHAIN=$HOME/Github/android-toolchain
+# export ANDROID_NDK=$HOME/AndroidDev/android-ndk-r9d/
+
+
 rm -rf build.android/ 
 mkdir build.android
 cd build.android
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DANDROID_ABI="armeabi" -DANDROID=1 ..
+
+make 
+
+cd ..
+rm -rf build.android/ 
+mkdir build.android/ 
+cd build.android/ 
+
+#build for armeabi-v7a
+cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DANDROID=1 ..
+make
+
+cd ..
+rm -rf build.android/ 
+mkdir build.android/ 
+cd build.android/ 
+
+#build for x86
+export PATH=$PATH:$HOME/Github/android-toolchain-x86/bin
+export ANDROID_STANDALONE_TOOLCHAIN=$HOME/Github/android-toolchain-x86
+cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DANDROID_ABI="x86" -DANDROID=1 ..
+
+make
+
+
+cd ..
+rm -rf build.android/ 
